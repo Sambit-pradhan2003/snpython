6faed7440ba2c94d62d11036c28fbe73f5ae94d6
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-03-21 23:45:26 +0530

    test

--- sn2.py
+++ sn2.py
@@ -5,5 +5,28 @@ def sn(n):
 sn("hello sn")
 
 
-mylist=[1,[1,2,3,'g'],'t']
-print(mylist)
+# mylist=[1,[1,2,3,'g'],'t']
+# print(mylist)
+
+
+# sn={ 
+#     "sn1":1,
+#     "sn2":2,
+#     "sn3":3,
+#     }
+# print(sn)
+# if "sn2" in sn:
+#      print("here")
+     
+     
+     
+# x="gkjgfkjghjhg"
+# print(len(x))
+ 
+ 
+sn="sambit"
+sn1=""
+for i in sn:
+    print(i)
+    sn1=i+sn1
+print(sn1)
